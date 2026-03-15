0b92a426e5c85a00e48e6fb6b2cad18b1fe04ba3
Will Kuntze <will.kuntze@gmail.com>
2026-03-15 02:00:25 -0400

    top right things

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 const plusElements = document.querySelectorAll('.smallPlus');
+const currentTimeElement = document.querySelector('.timeText');
+const currrentDateElement = document.querySelector('.dateText');
+
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    if (hours >= 12) {
+        currentTimeElement.textContent = `the current time is ${hours}:${minutes} PM`;
+    } else {
+        currentTimeElement.textContent = `the current time is ${hours}:${minutes} AM`;
+    }
+}
+
+function updateDate() {
+    const now = new Date();
+    const day = now.getDate();
+    currrentDateElement.textContent = `${day}`;
+}
+
+updateTime();
+setInterval(updateTime, 60000); // Update every minute
+updateDate();
+setInterval(updateDate, 86400000); // Update every day
+
 
 plusElements.forEach(plus => {
     const randomDelay = Math.random() * 5;
